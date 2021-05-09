6470665d125977709407dd0ca373e9207a9780f4
Micah <mbro36@gmail.com>
2021-05-08 23:49:50 -0600

    restful APIs

--- src/app.js
+++ src/app.js
@@ -5,10 +5,11 @@ const flips = require("./data/flips-data");
 const counts = require("./data/counts-data");
 app.use(express.json())
 
+//get the count of a specific result (heads,tails, edge)
 app.use("/counts/:countId", (req, res, next) => {
   const { countId } = req.params;
   const foundCount = counts[countId];
-
+  //check if the countId exists
   if (foundCount === undefined) {
     next(`Count id not found: ${countId}`);
   } else {
@@ -16,10 +17,12 @@ app.use("/counts/:countId", (req, res, next) => {
   }
 });
 
+//get the counts data
 app.use("/counts", (req, res) => {
   res.json({ data: counts });
 });
 
+// get the results of a specific flip
 app.use("/flips/:flipId", (req, res, next) => {
   const { flipId } = req.params;
   const foundFlip = flips.find((flip) => flip.id === Number(flipId));
@@ -30,6 +33,7 @@ app.use("/flips/:flipId", (req, res, next) => {
   }
 });
 
+//get the results of all the flips
 app.get("/flips", (req, res) => {
   res.json({ data: flips });
 });
@@ -38,15 +42,20 @@ app.get("/flips", (req, res) => {
 // Since some ID's may already be used, you find the largest assigned id.
 let lastFlipId = flips.reduce((maxId, flip) => Math.max(maxId, flip.id), 0);
 
+// post new flip results
 app.post("/flips", (req, res, next) => {
   const { data: { result } = {} } = req.body;
-  const newFlip = {
-    id: ++lastFlipId, // Increment last id then assign as the current ID
-    result,
-  };
-  flips.push(newFlip);
-  counts[result] = counts[result] + 1; // Increment the counts
-  res.json({ data: newFlip });
+  if (result){
+    const newFlip = {
+      id: ++lastFlipId, // Increment last id then assign as the current ID
+      result,
+    };
+    flips.push(newFlip);
+    counts[result] = counts[result] + 1; // Increment the counts
+    res.status(201).json({ data: newFlip });
+  } else {
+    res.sendStatus(400);
+  }
 });
 
 // Not found handler
